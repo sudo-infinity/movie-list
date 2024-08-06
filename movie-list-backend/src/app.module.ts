730b9c62a94a3baec7d61bc5678fae4b52dc3f3f
sudo-infinity <aminchaudhry.dev@gmail.com>
2024-08-05 17:14:38 -0700

    feat: add postgress

--- movie-list-backend/src/app.module.ts
+++ movie-list-backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { MovieModule } from './movie/movie.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    PrismaModule,
+    UserModule,
+    AuthModule,
+    MovieModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- movie-list-backend/src/main.ts
+++ movie-list-backend/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL, // Your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // If you need to include cookies in requests
+  });
+
+  await app.listen(process.env.PORT, () => {
+    console.log(`Application is running on: ${process.env.PORT}`);
+  });
 }
 bootstrap();

78e438a5cc224413612a1c51b77247695672a051
sudo-infinity <aminchaudhry.dev@gmail.com>
2024-08-05 18:50:36 -0700

    feat: add auth and cloudinary

---
+++ movie-list-backend/src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
+import { UserModule } from '../user/user.module';
+import { JwtAuthGuard } from './jwt-auth.guard';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '3h' },
+    }),
+    UserModule,
+  ],
+  providers: [JwtStrategy, JwtAuthGuard],
+  exports: [JwtModule, JwtAuthGuard],
+})
+export class AuthModule {}
---
+++ movie-list-backend/src/cloudinary/cloudinary.service.ts
@@ -0,0 +1,28 @@
+// src/cloudinary/cloudinary.service.ts
+import { Injectable } from '@nestjs/common';
+import * as cloudinary from 'cloudinary';
+
+@Injectable()
+export class CloudinaryService {
+  constructor() {
+    cloudinary.v2.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+  }
+
+  async uploadImage(file: Express.Multer.File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const uploadStream = cloudinary.v2.uploader.upload_stream(
+        { folder: '/movies-api' },
+        (error, result) => {
+          if (error) reject(error);
+          else resolve(result.url);
+        },
+      );
+
+      uploadStream.end(file.buffer);
+    });
+  }
+}
